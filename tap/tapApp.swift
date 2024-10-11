09e37272be9582b752e0e462a0e2f147758bb053
Aadi Katyal <52411543+aadikatyal@users.noreply.github.com>
2024-10-10 20:15:55 -0400

    Initial Commit

---
+++ tap/tapApp.swift
@@ -0,0 +1,17 @@
+//
+//  tapApp.swift
+//  tap
+//
+//  Created by Aadi Katyal on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct tapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tapTests/tapTests.swift
@@ -0,0 +1,17 @@
+//
+//  tapTests.swift
+//  tapTests
+//
+//  Created by Aadi Katyal on 10/10/24.
+//
+
+import Testing
+@testable import tap
+
+struct tapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
